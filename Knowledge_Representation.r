00872cc1ab4207afcd9f3fd8fd564f2c14c63cd5
ankitayyappan007 <126398943+ankitayyappan007@users.noreply.github.com>
2026-03-25 20:15:46 +0530

    Refactor fraud analysis and add summary table
    Added summary table for fraud analysis by state, payment method, merchant, and time.

--- Knowledge_Representation.r
+++ Knowledge_Representation.r
@@ -2,49 +2,50 @@
 library(dplyr)
 
 fraud_rate_by_state <- fact_transactions %>%
-  left_join(dim_location, by="location_key") %>%
+  left_join(dim_location, by = "location_key") %>%
   group_by(state) %>%
-  summarise(
-    transactions = sum(txn_count),
-    fraud_cases = sum(is_fraud),
-    fraud_rate = fraud_cases / transactions
-  ) %>%
+  summarise(transactions = sum(txn_count),
+            fraud_cases = sum(is_fraud),
+            fraud_rate = fraud_cases / transactions) %>%
   arrange(desc(fraud_rate))
 
 print(fraud_rate_by_state)
 
 fraud_by_payment <- fact_transactions %>%
-  left_join(dim_payment, by="payment_key") %>%
+  left_join(dim_payment, by = "payment_key") %>%
   group_by(payment_method) %>%
-  summarise(
-    transactions = sum(txn_count),
-    fraud_cases = sum(is_fraud),
-    fraud_rate = fraud_cases / transactions
-  ) %>%
+  summarise(transactions = sum(txn_count),
+            fraud_cases = sum(is_fraud),
+            fraud_rate = fraud_cases / transactions) %>%
   arrange(desc(fraud_rate))
 
 print(fraud_by_payment)
 
 fraud_by_merchant <- fact_transactions %>%
-  left_join(dim_merchant, by="merchant_key") %>%
+  left_join(dim_merchant, by = "merchant_key") %>%
   group_by(merchant_category) %>%
-  summarise(
-    transactions = sum(txn_count),
-    fraud_cases = sum(is_fraud),
-    fraud_rate = fraud_cases / transactions
-  ) %>%
+  summarise(transactions = sum(txn_count),
+            fraud_cases = sum(is_fraud),
+            fraud_rate = fraud_cases / transactions) %>%
   arrange(desc(fraud_rate))
 
 print(fraud_by_merchant)
 
 fraud_by_hour <- fact_transactions %>%
-  left_join(dim_time, by="time_key") %>%
+  left_join(dim_time, by = "time_key") %>%
   group_by(hour) %>%
-  summarise(
-    transactions = sum(txn_count),
-    fraud_cases = sum(is_fraud),
-    fraud_rate = fraud_cases / transactions
-  ) %>%
+  summarise(transactions = sum(txn_count),
+            fraud_cases = sum(is_fraud),
+            fraud_rate = fraud_cases / transactions) %>%
   arrange(desc(fraud_rate))
 
 print(fraud_by_hour)
+
+summary_table <- list(
+  state = fraud_rate_by_state,
+  payment = fraud_by_payment,
+  merchant = fraud_by_merchant,
+  time = fraud_by_hour
+)
+
+print(summary_table)
